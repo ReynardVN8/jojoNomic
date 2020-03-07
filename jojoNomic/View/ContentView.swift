4e46052fe27e96caf39902880957c4410cf69579
ReynardVN8 <reynardvnata@yahoo.co.id>
2020-03-07 23:00:47 +0700

    Deleting for efficiency of an app

--- jojoNomic/View/ContentView.swift
+++ jojoNomic/View/ContentView.swift
@@ -10,11 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var body: some View {
-        VStack{
-            CurrentWeather()
+    @ObservedObject var locationManager = LocationManager()
+
+        var userLatitude: Double {
+            return locationManager.lastLocation?.coordinate.latitude ?? 0
         }
-    }
+
+        var userLongitude: Double {
+            return locationManager.lastLocation?.coordinate.longitude ?? 0
+        }
+        
+        var weather: String {
+            return WeatherGetter().getWeather(lat: userLatitude, long: userLongitude)
+    //        return WeatherGetter().searchedCity(city: "Jakarta")
+        }
+        
+        
+        var body: some View {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color(red: 0.6, green: 1, blue: 0.95, opacity: 1))
+                    .edgesIgnoringSafeArea(.all)
+                    VStack{
+                        Text("Your Location coordinate: latitude, longitude").padding()
+                        Text("\(userLatitude), \(userLongitude)").padding()
+                        Text(weather).padding()
+                }.padding()
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
